cc7ab2e06a5303c202186aca2f61554c86531efc
pla <massenpost@pla1.de>
2025-08-18 10:58:24 +0200

    Fix tu-supplier and tu-receiver recipe

--- train-upgrader/prototypes/recipe.lua
+++ train-upgrader/prototypes/recipe.lua
@@ -1,29 +1,29 @@
 data:extend({
-  {
-    type = "recipe",
-    name = "tu-supplier",
-    enabled = false,
-	  energy_required = 5,
-    ingredients = { 
-      {"passive-provider-chest", 1},
-      {"rail-signal", 1},
-      {"construction-robot", 1}
-    },
-    results = {
-      {type = "item", name = "tu-supplier", amount = 1}
-    }
-  },
-  {
-    type = "recipe",
-    name = "tu-receiver",
-    enabled = false,
-	  energy_required = 4,
-    ingredients = {
-      {"storage-chest", 1},
-      {"rail-chain-signal", 1}
-    },
-    results = {
-      {type = "item", name = "tu-receiver", amount = 1}
-    }
-  }
+	{
+		type = "recipe",
+		name = "tu-supplier",
+		enabled = false,
+		energy_required = 5,
+		ingredients = {
+			{ type = "item", name = "passive-provider-chest", amount = 1 },
+			{ type = "item", name = "rail-signal", amount = 1 },
+			{ type = "item", name = "construction-robot", amount = 1 },
+		},
+		results = {
+			{ type = "item", name = "tu-supplier", amount = 1 },
+		},
+	},
+	{
+		type = "recipe",
+		name = "tu-receiver",
+		enabled = false,
+		energy_required = 4,
+		ingredients = {
+			{ type = "item", name = "storage-chest", amount = 1 },
+			{ type = "item", name = "rail-chain-signal", amount = 1 },
+		},
+		results = {
+			{ type = "item", name = "tu-receiver", amount = 1 },
+		},
+	},
 })
