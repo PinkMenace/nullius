5284a963cd970a2d5dc1865f4a8530b809c604c2
Arthur Louvrier <arthur.louvrier@etu.minesparis.psl.eu>
2025-08-21 20:27:11 +0200

    Rebalance inserters: tier 1 can't filter anymore and following tiers can but have a new recipe with sensors/logic circuit

--- nullius/prototypes/override.lua
+++ nullius/prototypes/override.lua
@@ -991,3 +991,4 @@ for _, sc in pairs(data.raw.shortcut) do
 end
 
 data.raw["cargo-landing-pad"]["cargo-landing-pad"].localised_name = {"item-name.nullius-landing-pad"}
+data.raw["inserter"]["inserter"].filter_count = 0
