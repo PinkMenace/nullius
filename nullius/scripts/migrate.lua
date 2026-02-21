45d2b677f20e9276bd7b9c8bc19dbcabd9c0c3f8
Arthur Louvrier <arthur.louvrier@etu.minesparis.psl.eu>
2026-02-21 20:55:59 +0100

    Fixed bug when migrating valves

--- nullius/scripts/migrate.lua
+++ nullius/scripts/migrate.lua
@@ -146,6 +146,74 @@ function migrate_version(event)
   local version = parse_version(version_info.old_version)
   if (version == nil) then return end
 
+  if(version >= 20002) then return end
+  for _, surface in pairs(game.surfaces) do
+    for _, entity in pairs(surface.find_entities_filtered({ name = "nullius-one-way-valve" })) do
+        local position = entity.position
+        local direction = entity.direction
+        local force = entity.force
+        entity.destroy({ raise_destroy = true })
+    
+        local valve = surface.create_entity({
+            name = "nullius-configurable-valve",
+            position = position,
+            force = force,
+            direction = direction,
+            raise_built = true,
+        })
+        
+        local control_behavior = valve.get_or_create_control_behavior()
+        control_behavior.circuit_condition = { comparator = '>', first_signal = { type = "virtual", name = "signal-I" },  second_signal = { type = "virtual", name = "signal-O" }, }
+    end
+    for _, entity in pairs(surface.find_entities_filtered({ name = "nullius-relief-valve" })) do
+        local position = entity.position
+        local direction = entity.direction
+        local force = entity.force
+        entity.destroy({ raise_destroy = true })
+    
+        local valve = surface.create_entity({
+            name = "nullius-configurable-valve",
+            position = position,
+            force = force,
+            direction = direction,
+            raise_built = true,
+        })
+        local control_behavior = valve.get_or_create_control_behavior()
+        control_behavior.circuit_condition = { comparator = '>', first_signal = { type = "virtual", name = "signal-I" },  constant = 75, }
+    end
+    for _, entity in pairs(surface.find_entities_filtered({ name = "nullius-priority-valve" })) do
+        local position = entity.position
+        local direction = entity.direction
+        local force = entity.force
+        entity.destroy({ raise_destroy = true })
+    
+        local valve = surface.create_entity({
+            name = "nullius-configurable-valve",
+            position = position,
+            force = force,
+            direction = direction,
+            raise_built = true,
+        })
+        local control_behavior = valve.get_or_create_control_behavior()
+        control_behavior.circuit_condition = { comparator = '>', first_signal = { type = "virtual", name = "signal-I" },  constant = 25, }
+    end
+    for _, entity in pairs(surface.find_entities_filtered({ name = "nullius-top-up-valve" })) do
+        local position = entity.position
+        local direction = entity.direction
+        local force = entity.force
+        entity.destroy({ raise_destroy = true })
+    
+        local valve = surface.create_entity({
+            name = "nullius-configurable-valve",
+            position = position,
+            force = force,
+            direction = direction,
+            raise_built = true,
+        })
+        local control_behavior = valve.get_or_create_control_behavior()
+        control_behavior.circuit_condition = { comparator = '<', first_signal = { type = "virtual", name = "signal-O" }, constant = 50, }
+    end
+  end
   if(version >= 20000) then return end
   for _,bucket in pairs(storage.nullius_turbine_buckets) do
     for _,t in pairs(bucket.turbines) do
